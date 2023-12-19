693925a3610af5fa5fa4fe6b63a852cc4758d69f
NnnastyaA <nastyafedorets@mail.ru>
2023-12-19 21:45:22 +0300

    feat: add new code

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -141,8 +141,8 @@ function getLastDigit(value) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return parseFloat(value);
 }
 
 /**
@@ -158,8 +158,9 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  const diagonalLenght = a ** 2 + b ** 2 + c ** 2;
+  return Math.sqrt(diagonalLenght);
 }
 
 /**
@@ -234,8 +235,8 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num ** 3;
 }
 
 /**
